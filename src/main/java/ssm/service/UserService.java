136098ea76343787730d8ede304483fe657c4e52
yaoqiang <ketayao@gmail.com>
2017-08-27 21:53:16 +0800

    配置项目。

--- src/main/java/ssm/service/UserService.java
+++ src/main/java/ssm/service/UserService.java
@@ -6,5 +6,5 @@ import ssm.model.User;
  * Created by jzy on 2017/8/27.
  */
 public interface UserService {
-    User getUser(String userName, String password);
+    User getUser(String userName,String password);
 }
